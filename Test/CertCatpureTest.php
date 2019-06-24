55e3798f4a7043e3cb19c6cf180b479f92a0855c
WoodyXiong <kui.xiong@baishancloud.com>
2019-06-24 22:37:00 +0800

    feat: add .travis and phpunit in composer.json

---
+++ Test/CertCatpureTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CertCapture\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class CertCaptureTest extends TestCase
+{
+    public function testEqual()
+    {
+        $this->assertEquals(1,1);
+    }
+}
